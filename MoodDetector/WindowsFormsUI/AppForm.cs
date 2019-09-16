d5578d377fe41a03ac2218838812ac1d1168169d
Dilanas <sdilanas@gmail.com>
2019-09-16 13:28:21 +0300

    Task#1 deleted old database, new project with SQLServer

---
+++ MoodDetector/WindowsFormsUI/AppForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsUI
+{
+    public partial class AppForm : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MoodDetectorDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        public AppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTeacherButton_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("INSERT INTO Teacher(Firstname, Lastname, Subject) values('" + firstnameTextBox.Text + "','" + lastnameTextBox.Text + "','" + subjectTextBox.Text + "')", connection);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+        }
+    }
+}
