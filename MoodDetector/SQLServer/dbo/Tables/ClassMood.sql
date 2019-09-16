76498b8fb69f05c8a77b05f4a38c68b89c42ddfe
Dilanas Sabaliauskas <47833194+DilSab@users.noreply.github.com>
2019-09-16 20:33:00 +0300

    TASK#1 Database
    * Database and form to add values

---
+++ MoodDetector/SQLServer/dbo/Tables/ClassMood.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ClassMood]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [TeacherId] INT NOT NULL, 
+    [Class] NVARCHAR(5) NOT NULL, 
+    [AverageMood] INT NOT NULL, 
+    CONSTRAINT [FK_ClassMood_Teacher] FOREIGN KEY ([TeacherId]) REFERENCES [Teacher]([Id])
+)
---
+++ MoodDetector/SQLServer/dbo/Tables/Teacher.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Teacher]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Firstname] NVARCHAR(50) NOT NULL, 
+    [Lastname] NVARCHAR(50) NOT NULL, 
+    [Subject] NVARCHAR(50) NOT NULL
+)
