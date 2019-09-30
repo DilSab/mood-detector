ef89a8b9768ff3a72d4c2abff1371b72c636eca3
Agota <45971426+agosu@users.noreply.github.com>
2019-09-30 22:20:41 +0300

    Add affectiva (#12)
    * Added affectiva files plus initial classes
    
    * AffectivaService.GetEmotions returns a dictionary of nine emotions
    
    * Rename
    
    * Just
    
    * Get Expressions
    
    * Get Emojis
    
    * minor detail
    
    * Refactoring and affectiva_doc(notes) added
    
    * Refactoring
    
    * More refactoring
    
    * Removed affectiva .dll's from git
    
    * Added those spaces
    
    * More spaces added

---
+++ MoodDetector/Controller/Affectiva/AffectivaService.cs
@@ -0,0 +1,47 @@
+﻿using Affdex;
+using System.Collections.Generic;
+
+namespace Controller.Affectiva
+{
+    public class AffectivaService
+    {
+        readonly PhotoDetector detector;
+        readonly ImageListenerImpl imageListener;
+        readonly Helper helper;
+
+        AffectivaService()
+        {
+            detector = new PhotoDetector(1, FaceDetectorMode.LARGE_FACES);
+            imageListener = new ImageListenerImpl(detector);
+            helper = new Helper();
+            detector.setClassifierPath("C:\\Program Files\\Affectiva\\AffdexSDK\\data");
+            detector.setDetectAllExpressions(true);
+            detector.setDetectAllEmotions(true);
+            detector.setDetectAllEmojis(true);
+            detector.setDetectAllAppearances(false);
+        }
+
+        public void ProcessPhoto(string filePath)
+        {
+            Frame frame = helper.LoadFrameFromFile(filePath);
+            detector.start();
+            detector.process(frame);
+            detector.stop();
+        }
+
+        public Dictionary<string, float> GetEmotions()
+        {
+            return imageListener.GetEmotions();
+        }
+
+        public Dictionary<string, float> GetExpressions()
+        {
+            return imageListener.GetExpressions();
+        }
+
+        public Dictionary<string, float> GetEmojis()
+        {
+            return imageListener.GetEmojis();
+        }
+    }
+}
