a03b4be671a1d3797109725f83162068824e0e5d
Agota Suliokaite <agota.suliokaite@mif.stud.vu.lt>
2019-09-30 17:03:01 +0300

    Get Emojis

--- MoodDetector/Controller/Affectiva/AffectivaService.cs
+++ MoodDetector/Controller/Affectiva/AffectivaService.cs
@@ -1,9 +1,6 @@
 ﻿using Affdex;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
 
 namespace Controller.Affectiva
 {
@@ -30,13 +27,13 @@ namespace Controller.Affectiva
             detector.stop();
             return emotions;
         }
-
-        
     }
 
     public class ImageListenerImpl : ImageListener
     {
         Dictionary<string, float> emotions;
+        Dictionary<string, float> expressions;
+        Dictionary<string, float> emojis;
         public ImageListenerImpl(PhotoDetector detector)
         {
             detector.setImageListener(this);
@@ -48,6 +45,27 @@ namespace Controller.Affectiva
         }
 
         public void onImageResults(Dictionary<int, Face> faces, Frame frame)
+        {
+            AddEmotions(faces);
+            AddExpressions(faces);
+        }
+
+        public Dictionary<string, float> GetEmotions()
+        {
+            return emotions;
+        }
+
+        public Dictionary<string, float> GetExpressions()
+        {
+            return expressions;
+        }
+
+        public Dictionary<string, float> GetEmojis()
+        {
+            return emojis;
+        }
+
+        private void AddEmotions(Dictionary<int, Face> faces)
         {
             foreach (KeyValuePair<int, Face> face in faces)
             {
@@ -64,9 +82,53 @@ namespace Controller.Affectiva
             }
         }
 
-        public Dictionary<string, float> GetEmotions()
+        private void AddExpressions(Dictionary<int, Face> faces)
         {
-            return emotions;
+            foreach (KeyValuePair<int, Face> face in faces)
+            {
+                Expressions exp = face.Value.Expressions;
+                expressions.Add("attention", exp.Attention);
+                expressions.Add("brow furrow", exp.BrowFurrow);
+                expressions.Add("brow raise", exp.BrowRaise);
+                expressions.Add("cheek raise", exp.CheekRaise);
+                expressions.Add("chin raise", exp.ChinRaise);
+                expressions.Add("dimpler", exp.Dimpler);
+                expressions.Add("eye closure", exp.EyeClosure);
+                expressions.Add("eye widen", exp.EyeWiden);
+                expressions.Add("inner brow raise", exp.InnerBrowRaise);
+                expressions.Add("jaw drop", exp.JawDrop);
+                expressions.Add("lid tighten", exp.LidTighten);
+                expressions.Add("lip corner depressor", exp.LipCornerDepressor);
+                expressions.Add("lip press", exp.LipPress);
+                expressions.Add("lip pucer", exp.LipPucker);
+                expressions.Add("lip stretch", exp.LipStretch);
+                expressions.Add("lip suck", exp.LipSuck);
+                expressions.Add("mouth open", exp.MouthOpen);
+                expressions.Add("nose wrinkle", exp.NoseWrinkle);
+                expressions.Add("smile", exp.Smile);
+                expressions.Add("smirk", exp.Smirk);
+                expressions.Add("upper lip raise", exp.UpperLipRaise);
+            }
+        }
+
+        private void AddEmojis(Dictionary<int, Face> faces)
+        {
+            foreach (KeyValuePair<int, Face> face in faces)
+            {
+                Emojis em = face.Value.Emojis;
+                emojis.Add("disappointed", em.disappointed);
+                emojis.Add("flushed", em.flushed);
+                emojis.Add("kissing", em.kissing);
+                emojis.Add("laughing", em.laughing);
+                emojis.Add("rage", em.rage);
+                emojis.Add("relaxed", em.relaxed);
+                emojis.Add("scream", em.scream);
+                emojis.Add("smiley", em.smiley);
+                emojis.Add("smirk", em.smirk);
+                emojis.Add("stuck out tongue", em.stuckOutTongue);
+                emojis.Add("stuck out tongue winking eye", em.stuckOutTongueWinkingEye);
+                emojis.Add("wink", em.wink);
+            }
         }
     }
 }
