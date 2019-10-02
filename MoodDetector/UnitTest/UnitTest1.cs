2684fd70dbc1da6431563c3c10e5aef25950f1bd
Zygis0321 <55206909+Zygis0321@users.noreply.github.com>
2019-10-02 15:10:55 +0300

    created unit test project (#11)

---
+++ MoodDetector/UnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void FirstFact()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, 1, 2)]
+        public void FirstTheory(int add1, int add2, int expectedSum)
+        {
+            Assert.Equal(expectedSum, add1 + add2);
+        }
+    }
+}
