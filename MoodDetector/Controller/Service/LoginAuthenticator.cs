ee41d8af677e2b51f49499ca488a80f3e256cf4b
Dilanas Sabaliauskas <47833194+DilSab@users.noreply.github.com>
2019-10-13 19:39:27 +0300

    Authenticator tests (#21)

--- MoodDetector/Controller/Service/LoginAuthenticator.cs
+++ MoodDetector/Controller/Service/LoginAuthenticator.cs
@@ -1,22 +1,24 @@
 ﻿using Model;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 
 namespace Controller.Service
 {
     public class LoginAuthenticator : ILoginAuthenticator
     {
+        private MoodDetectorDBEntities _context;
+
+        public LoginAuthenticator(MoodDetectorDBEntities context)
+        {
+            _context = context;
+        }
+
         public bool IsLoginCorrect(string username, string password)
         {
-            using (MoodDetectorDBEntities context = new MoodDetectorDBEntities())
-            {
-                var user = context.LoginInfoes.FirstOrDefault(u => u.Username == username);
+                var user = _context.LoginInfoes.FirstOrDefault(u => u.Username == username);
                 if (user != null)
                 {
                     return user.Password == password ? true : false;
                 }
-            }
 
             return false;
         }
