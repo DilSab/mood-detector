050a9fb613050f559a3f35255dd2b0af3bc0b487
Agota Suliokaite <agota.suliokaite@mif.stud.vu.lt>
2019-10-26 15:09:41 +0300

    Initial commit, web app created and tried some things

---
+++ MoodDetector/WebApplication/ClientApp/src/components/AffectivaTest.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+
+export class AffectivaTest extends Component {
+    displayName = AffectivaTest.name
+
+    sayHello() {
+        alert(document.getElementById('file').files.item(0).name);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Upload photo:</h1>
+                <input id="file" type="file" />
+                <input type="submit" onClick={this.sayHello} />
+            </div>
+        );
+    }
+}
---
+++ MoodDetector/WebApplication/Controllers/AffectivaTestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Controller.Service;
+using Controller;
+
+namespace WebApplication.Controllers
+{
+    public class AffectivaTestController : Controller
+    {
+        public class AffectivaTestPage
+        {
+            public void checkPhoto()
+            {
+
+            }
+
+
+        }
+    }
+}
