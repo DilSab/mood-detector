1328ac9667e9166b823283992d93c56e4ce49563
Dilanas <sdilanas@gmail.com>
2019-11-01 22:23:09 +0200

    Deleted libraries

--- MoodDetector/MoodDetectorWebApp/Scripts/moodlistRedirect.js
+++
@@ -1,9 +0,0 @@
-﻿$(function () {
-    $("#show-moodlist-btn").click(function (event) {
-        event.preventDefault();
-        inputValue = $("#user-id-input").val();
-        if ($.isNumeric(inputValue)) {
-            window.location.href = "@Url.Action("GetMoodList", "Mood")" + "/" + $("#user-id-input").val();
-        }
-    });
-});
