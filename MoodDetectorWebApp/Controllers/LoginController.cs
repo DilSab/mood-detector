3d21214ee46458a2e1dbe211789871a5278f6598
Paulius Tartilas <paulius.tartilas@mif.stud.vu.lt>
2019-11-05 23:12:14 +0200

    Login page draft

--- MoodDetectorWebApp/Controllers/LoginController.cs
+++ MoodDetectorWebApp/Controllers/LoginController.cs
@@ -1,13 +1,52 @@
 ﻿using System.Web.Mvc;
+using ControllerProject;
+using ControllerProject.Service;
+using MoodDetectorWebApp.Models;
 
 namespace MoodDetectorWebApp.Controllers
 {
     public class LoginController : Controller
     {
+        private ILoginProcessor _loginProcessor;
+        private IUserService _userService;
+        private IMoodService _moodService;
+        
+        public LoginController(ILoginProcessor loginProcessor, IUserService userService, IMoodService moodService)
+        {
+            _loginProcessor = loginProcessor;
+            _userService = userService;
+            _moodService = moodService;
+        }
+
+
         // GET: Login
         public ActionResult Login()
         {
+            return View(new LoginModel());
+        }
+
+        // POST: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            bool loginCorrect = _loginProcessor.ProcessLogin(model.UserName, model.LoginInfo.Password);
+
+            if (loginCorrect) {
+                var user = _userService.GetUser(model.UserName);
+                switch (user.AccessRights)
+                {
+                    case "Admin":
+                        return View("~/Views/MyProfileAdmin/MyProfileAdmin.cshtml");
+                       
+                    case "Teacher":
+                        return View("~/Views/MyProfileTeacher/MyProfileTeacher.csthml");                        
+                }
+            }
+
             return View();
         }
+
+
     }
 }
