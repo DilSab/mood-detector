99778e4caad791f806ca49ef09b0fe915ca1696a
Agota <45971426+agosu@users.noreply.github.com>
2019-11-06 13:44:18 +0200

    Admin manage users (#34)
    * Added adduser functionality
    
    * Button instead of link again

---
+++ MoodDetectorWebApp/Models/AddUserModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoodDetectorWebApp.Models
+{
+    public class AddUserModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 4)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 10)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Lastname { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 5)]
+        public string AccessRights { get; set; }
+    }
+}
