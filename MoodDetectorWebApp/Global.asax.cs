3bde5f80c5531e433512dbc79c7093a35808248f
Paulius Tartilas <paulius.tartilas@mif.stud.vu.lt>
2019-11-16 14:07:48 +0200

    Deleted comments

--- MoodDetectorWebApp/Global.asax.cs
+++ MoodDetectorWebApp/Global.asax.cs
@@ -27,8 +27,7 @@ namespace MoodDetectorWebApp
                 if (Request.Cookies[FormsAuthentication.FormsCookieName] != null)
                 {
                     try
-                    {
-                        //let us take out the username now                
+                    {                                     
                         string username = FormsAuthentication.Decrypt(Request.Cookies[FormsAuthentication.FormsCookieName].Value).Name;
                         string roles = string.Empty;
 
@@ -38,17 +37,14 @@ namespace MoodDetectorWebApp
 
                             roles = user.User.AccessRights;
                         }
-                        //let us extract the roles from our own custom cookie
-
-                        
-                        //Let us set the Pricipal with our user specific details
+                  
                         HttpContext.Current.User = new System.Security.Principal.GenericPrincipal(
                         new System.Security.Principal.GenericIdentity(username, "Forms"), roles.Split(';'));
                         
                     }
                     catch (Exception)
                     {
-                        //somehting went wrong
+                       
                     }
                 }
             }
