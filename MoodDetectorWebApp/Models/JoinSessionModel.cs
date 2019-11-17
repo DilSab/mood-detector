c9ad9dec11040b05450cdac1d607db2bf33bb60a
Paulius Tartilas <paulius.tartilas@mif.stud.vu.lt>
2019-11-17 15:21:44 +0200

    Initial Controller+View+Model+Layout Meniu

---
+++ MoodDetectorWebApp/Models/JoinSessionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoodDetectorWebApp.Models
+{
+    public class JoinSessionModel
+    {
+        public int DetectionId { get; set; }
+
+        public string JoinId { get; set; }
+    }
+}
