082cc6e2044f4c83f9ac0edffeb67483674c3f60
Paulius Tartilas <paulius.tartilas@mif.stud.vu.lt>
2019-11-18 12:26:12 +0200

    Added affectiva live camera detector

--- MoodDetectorWebApp/Controllers/JoinSessionController.cs
+++ MoodDetectorWebApp/Controllers/JoinSessionController.cs
@@ -1,17 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using ControllerProject.Service;
+using MoodDetectorWebApp.Models;
+using System;
+using Model.Entity;
+using Model;
 
 namespace MoodDetectorWebApp.Controllers
 {
     public class JoinSessionController : Controller
     {
-        // GET: JoinSession
-        public ActionResult JoinSession ()
-        {
+            private IMoodService _moodService;
+            private IUserService _userService;
+
+            public JoinSessionController(IMoodService moodService, IUserService userService)
+            {
+                _moodService = moodService;
+                _userService = userService;
+            }
+      
+        
+           // GET: JoinSession
+           public ActionResult JoinSession ()
+           {
+               return View(new JoinSessionModel());
+           }
+
+           
+           // POST: JoinSession
+           [HttpPost]
+           public ActionResult JoinSession(JoinSessionModel model)
+           {
+
+            // Link with database
+            
+            if (ModelState.IsValid)
+            {
+                return View("~/Views/JoinSession/Session.cshtml", model);
+            }
             return View();
-        }
+           }          
     }
 }
--- MoodDetectorWebApp/Models/JoinSessionModel.cs
+++ MoodDetectorWebApp/Models/JoinSessionModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MoodDetectorWebApp.Models
 {
     public class JoinSessionModel
     {
+        [Required]
+        [Range(1000, 9999, ErrorMessage = "Session ID should be between {1} and {2}.")]
         public int DetectionId { get; set; }
-
+        [Required(ErrorMessage = "Enter your name to join. Name should be 2-15 letters long")]
+        [StringLength(15, MinimumLength = 2)]
         public string JoinId { get; set; }
     }
 }
