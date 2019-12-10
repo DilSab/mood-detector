d9dd42fa2ace8705f2719b3e72e4ae8957f05171
Dilanas <sdilanas@gmail.com>
2019-12-10 20:48:12 +0200

    Aggregate and group

---
+++ UnitTest/Service/AggregateTest/AggregateTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace UnitTest.Service.AggregateTest
+{
+    public class AggregateTest
+    {
+        [Fact]
+        public void TestAggregate()
+        {
+            int[] numbers = { 84, 541, 2, 1, 326, 1111, 32, 9 };
+
+            Assert.Equal(1, numbers.Aggregate((lowestNumber, number) => number < lowestNumber ? number : lowestNumber));
+            Assert.Equal(263.25, (double) numbers.Aggregate((n1, n2) => n1 + n2) / numbers.Count());
+        }
+    }
+}
