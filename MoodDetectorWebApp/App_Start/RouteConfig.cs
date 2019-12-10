cf258f60f3bb4ce8de7fafb07b5b1e5c88b68f5c
Agota Suliokaite <agota.suliokaite@mif.stud.vu.lt>
2019-12-10 23:16:44 +0200

    Fixed some more

--- MoodDetectorWebApp/App_Start/RouteConfig.cs
+++ MoodDetectorWebApp/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MoodDetectorWebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "About", action = "About", id = UrlParameter.Optional }
+                defaults: new { controller = "About", action = "FancyAbout", id = UrlParameter.Optional }
             );
         }
     }
