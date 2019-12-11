f14dc59de45ae15450bfcb19737ba1b33c4af299
Zygis <zygimantas.remeika@gmail.com>
2019-12-11 19:24:27 +0200

    fixed

--- Model/Migrations/201912111719019_JoinSessionVideoID.Designer.cs
+++ Model/Migrations/201912111719019_JoinSessionVideoID.Designer.cs
@@ -7,18 +7,18 @@ namespace Model.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.3.0")]
-    public sealed partial class JoinSessionSessionVideoID : IMigrationMetadata
+    public sealed partial class JoinSessionVideoID : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(JoinSessionSessionVideoID));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(JoinSessionVideoID));
         
         string IMigrationMetadata.Id
         {
-            get { return "201912111055178_JoinSession + Session.VideoID"; }
+            get { return "201912111719019_JoinSessionVideoID"; }
         }
         
         string IMigrationMetadata.Source
         {
-            get { return null; }
+            get { return Resources.GetString("Source"); }
         }
         
         string IMigrationMetadata.Target
